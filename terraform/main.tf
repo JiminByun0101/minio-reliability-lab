007938305a5d98aaedf5ff9684c4e93ea9062f91
JiminByun0101 <jbyun0101@gmail.com>
2025-11-04 17:46:26 -0800

    refine network setup and SSH configuration for MinIO lab

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,67 @@
+# 1. Create a dedicated VPC
+resource "google_compute_network" "lab_vpc" {
+  name                    = "minio-lab-vpc"
+  auto_create_subnetworks = false
+}
+
+# 2. Create a subnet for your region
+resource "google_compute_subnetwork" "lab_subnet" {
+  name          = "minio-lab-subnet"
+  ip_cidr_range = "10.128.0.0/20"
+  region        = var.region
+  network       = google_compute_network.lab_vpc.id
+}
+
+# 3. Allow internal communication between all nodes
+resource "google_compute_firewall" "allow_internal" {
+  name    = "allow-internal"
+  network = google_compute_network.lab_vpc.id
+
+  # Kubernetes control plane
+  allow {
+    protocol = "tcp"
+    ports    = ["6443", "2379-2380", "10250-10252"]
+  }
+  # MinIO cluster ports (9000–9001 for API, 9002 for console)
+  allow {
+    protocol = "tcp"
+    ports    = ["9000-9002"]
+  }
+
+  source_ranges = ["10.128.0.0/20"]
+}
+
+# 4. Allow external SSH and API access
+resource "google_compute_firewall" "allow_external" {
+  name    = "allow-external"
+  network = google_compute_network.lab_vpc.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "6443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# 5. Provision the MinIO node instances
 resource "google_compute_instance" "minio_node" {
   count        = var.node_count
   name         = "minio-node-${count.index}"
-  machine_type = "e2-micro" 
+  machine_type = "e2-standard-2"
+
   allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
-      size  = 10
+      size  = 100
     }
   }
 
   network_interface {
-    network = "default"
+    network    = google_compute_network.lab_vpc.id
+    subnetwork = google_compute_subnetwork.lab_subnet.id
     access_config {}
   }
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,14 @@
+output "vpc_network" {
+  description = "Name of the VPC network"
+  value       = google_compute_network.lab_vpc.name
+}
+
+output "subnet_cidr" {
+  description = "Subnet CIDR range"
+  value       = google_compute_subnetwork.lab_subnet.ip_cidr_range
+}
+
 output "node_ips" {
-  description = "Public IPs MinIO nodes"
+  description = "Public IPs of MinIO nodes"
   value       = google_compute_instance.minio_node[*].network_interface[0].access_config[0].nat_ip
 }
